d7f9e0c0475081a6976143b14431295ddc1b9822
Devesh B <65714343+devesh-b@users.noreply.github.com>
2024-09-03 21:30:28 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,6 +1,3 @@
-# SPDX-FileCopyrightText: © 2024 Tiny Tapeout
-# SPDX-License-Identifier: Apache-2.0
-
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
@@ -25,16 +22,78 @@ async def test_project(dut):
 
     dut._log.info("Test project behavior")
 
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+    # Set the input values you want to test //a1
+    dut.ui_in.value = 0
+    dut.uio_in.value = 62
 
     # Wait for one clock cycle to see the output values
     await ClockCycles(dut.clk, 1)
+    
+   # Set the input values you want to test //b1
+    dut.ui_in.value = 0
+    dut.uio_in.value = 65
 
+    # Wait for one clock cycle to see the output values
+    await ClockCycles(dut.clk, 1)
+    
+   # Set the input values you want to test //a2
+    dut.ui_in.value = 0
+    dut.uio_in.value = 64
+   # Wait for one clock cycle to see the output values
+    await ClockCycles(dut.clk, 1)
+    
+   # Set the input values you want to test //b2
+    dut.ui_in.value = 0
+    dut.uio_in.value = 62
+    await ClockCycles(dut.clk, 1)
+    
+   # Set the input values you want to test //a3
+    dut.ui_in.value = 0
+    dut.uio_in.value = 66
+    await ClockCycles(dut.clk, 1)
+    
+   # Set the input values you want to test //b3
+    dut.ui_in.value = 0
+    dut.uio_in.value = 62
+
+    await ClockCycles(dut.clk, 1)
+    assert dut.uo_out.value == 65
+   # Set the input values you want to test //a4
+    dut.ui_in.value = 0
+    dut.uio_in.value = 64
+   # Wait for one clock cycle to see the output values
+    await ClockCycles(dut.clk, 1)
+    assert dut.uo_out.value == 0
+   # Set the input values you want to test //b4
+    dut.ui_in.value = 0
+    dut.uio_in.value = 62
+    await ClockCycles(dut.clk, 1)
+    assert dut.uo_out.value == 66
+     # Set the input values you want to test //a5
+    dut.ui_in.value = 0
+    dut.uio_in.value = 66
+   # Wait for one clock cycle to see the output values
+    await ClockCycles(dut.clk, 1)
+    assert dut.uo_out.value == 128
+   # Set the input values you want to test //b5
+    dut.ui_in.value = 0
+    dut.uio_in.value = 62
+    
     # The following assersion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+    
+    await ClockCycles(dut.clk, 1)
+    assert dut.uo_out.value == 68
+    await ClockCycles(dut.clk, 1)
+    assert dut.uo_out.value == 64
+    await ClockCycles(dut.clk, 1)
+    assert dut.uo_out.value == 68
+    await ClockCycles(dut.clk, 1)
+    assert dut.uo_out.value == 192
+    await ClockCycles(dut.clk, 1)
+    assert dut.uo_out.value == 69
+    await ClockCycles(dut.clk, 1)
+    assert dut.uo_out.value == 192
 
     # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+    # one or more clock cycles, and asserting the expected output values

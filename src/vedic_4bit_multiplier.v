24ba2257aaf2bda1079e7b1c6c52d5635cc5b647
Devesh B <65714343+devesh-b@users.noreply.github.com>
2024-09-04 22:07:13 +0530

    Update vedic_4bit_multiplier.v

--- src/vedic_4bit_multiplier.v
+++ src/vedic_4bit_multiplier.v
@@ -7,6 +7,7 @@ module vedic_4bit_multiplier (
 
     // Intermediate partial products
     wire [3:0] P0, P1, P2, P3;
+    wire temp;
 
     // 2x2 Vedic Multipliers for 4x4 multiplication
     vedic_2bit_multiplier U0 (A[1:0], B[1:0], P0[3:0]);
@@ -16,8 +17,6 @@ module vedic_4bit_multiplier (
 
     buf b[1:0](P[1:0], P0[1:0]);
 	 
-	 reversible_6bit_adder a1({P3, P0[3:2]}, {2'b0, P2}, {2'b0, P1}, P[7:2]);
+	 reversible_6bit_adder a1({P3, P0[3:2]}, {2'b0, P2}, {2'b0, P1}, P[7:2], temp);
 
 endmodule
-
-

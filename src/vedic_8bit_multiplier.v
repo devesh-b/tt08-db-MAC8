9f496c8a4ab773356a81ecec7168a1f5b877a224
Devesh B <65714343+devesh-b@users.noreply.github.com>
2024-09-04 22:14:44 +0530

    Update vedic_8bit_multiplier.v

--- src/vedic_8bit_multiplier.v
+++ src/vedic_8bit_multiplier.v
@@ -5,7 +5,7 @@ module vedic_8bit_multiplier (
 );
 
     // Intermediate partial products
-    wire [15:0] P0, P1, P2, P3;
+	wire [7:0] P0, P1, P2, P3;
 
     // 4x4 multipliers for partial products
     vedic_4bit_multiplier U0 (A[3:0], B[3:0], {P0[7:4], P[3:0]});

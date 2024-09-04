37d97a7b87ff73f67af8512a7bada287ec05871d
Devesh B <65714343+devesh-b@users.noreply.github.com>
2024-09-04 21:44:58 +0530

    Update reversible_16bit_adder.v

--- src/reversible_16bit_adder.v
+++ src/reversible_16bit_adder.v
@@ -2,15 +2,14 @@ module reversible_16bit_adder (
     input  wire [15:0] A,   // 6-bit input A
     input  wire [15:0] B,   // 6-bit input B
     input  wire        Cin,   // 6-bit input C
-    output reg [15:0] Sum, // 6-bit Sum
-    output reg        Carry // 6-bit Carry
+    output reg [15:0] Sum
 );
 
 // Intermediate wires for full adders
-wire [2:0]C;
+    wire [3:0]C;
 
 // First stge of full adders
-reversible_4bit_adder fa[3:0](A[15:0], B[15:0], {C[2:0], Cin}, Sum[15:0], {Carry, C[2:0]});
+    reversible_4bit_adder fa[3:0](A[15:0], B[15:0], {C[2:0], Cin}, Sum[15:0], {C[3:0]});
 
 
 endmodule

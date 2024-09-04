b6467aeea3bf3d1b35cdca2f1096986f869d2a9b
Devesh B <65714343+devesh-b@users.noreply.github.com>
2024-09-04 22:40:19 +0530

    Update reversible_4bit_adder.v

--- src/reversible_4bit_adder.v
+++ src/reversible_4bit_adder.v
@@ -9,8 +9,14 @@ module reversible_4bit_adder (
 // Intermediate wires for full adders
 wire [2:0] C;
 
-// First stage of full adders
-reversible_full_adder fa[3:0](A[3:0], B[3:0], {C[2:0], Cin}, 1'b0, Sum[3:0], {Carry, C[2:0]});
+// Instantiation of reversible full adders
+    reversible_full_adder fa0 (A[0], B[0], Cin,  1'b0, Sum[0], C[0]);
+    reversible_full_adder fa1 (A[1], B[1], C[0], 1'b0, Sum[1], C[1]);
+    reversible_full_adder fa2 (A[2], B[2], C[1], 1'b0, Sum[2], C[2]);
+    reversible_full_adder fa3 (A[3], B[3], C[2], 1'b0, Sum[3], Carry);
+
+endmodule
+
 
 
 endmodule

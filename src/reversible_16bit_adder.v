419b9b36145da3a7af3f3e070bfe17b6c786759e
Devesh B <65714343+devesh-b@users.noreply.github.com>
2024-09-04 22:19:58 +0530

    Update reversible_16bit_adder.v

--- src/reversible_16bit_adder.v
+++ src/reversible_16bit_adder.v
@@ -11,5 +11,5 @@ module reversible_16bit_adder (
 // First stge of full adders
     reversible_4bit_adder fa[3:0](A[15:0], B[15:0], {C[2:0], Cin}, Sum[15:0], {C[3:0]});
 
-    wire _unused = C[3];
+    wire _unused = &{C[3]};
 endmodule
